f5a794ec5498138aaec026a191fd8ba05c84fecd
Y.W <stewchicken@gmail.com>
2017-07-13 21:50:30 +0200

    update

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -9,8 +9,6 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
 export class GalleryPage {
 
   images: Array<string>;
-  //grid: Array<Array<string>>;
-  grid: Array<string>;
   photoFrames: Array<string>;
 
   constructor(
@@ -31,32 +29,49 @@ export class GalleryPage {
     */
   }
 
+  send() {
+    let checkmessage = "photo ";
+    let notselected = false;
+    for (let i = 0; i < this.photoFrames.length; i++) {
+      if (!this.photoFrames[i]) {
+        notselected = true;
+        checkmessage = checkmessage + (i+1) + " , "
+      }
+    }
+
+    if (notselected) {
+      checkmessage = checkmessage + " frame are not  selected !"
+
+      let alert = this.alertCtrl.create({
+        title: ' select frames ',
+        message: checkmessage,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Buy',
+            handler: () => {
+              console.log('Buy clicked');
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+
+
+
+  }
+
   ionViewDidLoad() {
     this.images = this.navParams.get('images');
-    this.grid = Array(Math.ceil(this.images.length / 2));
     this.photoFrames = Array(this.images.length);
 
-    /*
-    let rowNum = 0;
- 
-     for (let i = 0; i < this.images.length; i += 2) {
- 
-       if (i + 2 <= this.images.length) {
-         this.grid[rowNum] = Array(2);
-       } else {
-         this.grid[rowNum] = Array(1);
-       }
-       if (this.images[i]) {
-         this.grid[rowNum][0] = this.images[i]
-       }
- 
-       if (this.images[i + 1]) {
-         this.grid[rowNum][1] = this.images[i + 1]
-       }
- 
-       rowNum++;
-     }
-     */
   }
 
 }
